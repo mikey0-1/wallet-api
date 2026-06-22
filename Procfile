61613b722c82d4d72ad52c9f48b190812948ada3
mikey0-1 <mikebaliscao0@gmail.com>
2026-06-22 12:55:58 +0800

    for deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn WalletAPI.wsgi --bind 0.0.0.0:$PORT
--- WalletAPI/settings.py
+++ WalletAPI/settings.py
@@ -57,6 +57,7 @@ REST_FRAMEWORK = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -136,5 +137,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
